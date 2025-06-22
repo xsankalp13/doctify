0f63cd0f3b69629e9d263ca90b22ebc7efb3d764
xsankalp13 <sankalpbsalve@gmail.com>
2025-06-23 00:50:56 +0530

    Feat: Added the modal functionlity and the medical-agent/sessionId?=sessionId api

--- app/(routes)/dashboard/medical-agent/[sessionId]/page.tsx
+++ app/(routes)/dashboard/medical-agent/[sessionId]/page.tsx
@@ -1,11 +1,37 @@
 "use client"
+import axios from 'axios'
 import { useParams } from 'next/navigation'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+type SessionDetails = {
+  id: number,
+  notes: string,
+  sessionId: string,
+  report: JSON,
+  selectedDoctor: string,
+  createdAt: Date,
+
+
+}
+
 
 const page = () => {
   const { sessionId } = useParams()
+  const [sessionDetails, setSessionDetails] = useState<SessionDetails>()
+
+
+  const getSessionDetails = async () => {
+    const res = await axios.get('/api/session-chat?sessionId='+sessionId);
+    console.log(res.data)
+    setSessionDetails(res.data.data ?? res.data); 
+  }
+
+  useEffect(() => {
+      sessionId && getSessionDetails()
+  },[sessionId])
+
   return (
-    <div>{sessionId}</div>
+    <div >{sessionDetails?.notes}</div>
   )
 }
 
